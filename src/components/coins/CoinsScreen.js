0f01d93e4cbac361bda589db7a141dbf0d2bd2e2
Brian Sanchez <briansanchezizq@gmail.com>
2021-06-24 14:53:47 -0500

    implement stack navigation for coinScreen and coinDetailScreen

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,9 +1,16 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-const CoinsScreen = () => {
+import React from 'react'
+import { View, Text, Pressable } from 'react-native'
+import tw from 'tailwind-react-native-classnames'
+const CoinsScreen = (props) => {
+    const handlePress = () => {
+        props.navigation.navigate('CoinDetail')
+    }
     return(
-        <View>
-            <Text>Coins Screen</Text>
+        <View style={tw`flex items-center h-full bg-gray-300 `}>
+            <Text style={tw`font-bold text-xl font-bold py-2`}>Coins screen</Text>
+            <Pressable onPress={ handlePress } style={tw`rounded flex justify-center mt-5 w-11/12 bg-blue-500 py-2`}>
+                <Text style={tw`text-base font-semibold text-white text-center`}>Ir al detalle</Text>
+            </Pressable>
         </View>
     )
 }
