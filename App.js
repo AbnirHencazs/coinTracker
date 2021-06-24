04ba1437d9b842a09ae63e4896e171c1e1f7cc4a
Brian Sanchez <briansanchezizq@gmail.com>
2021-06-24 13:49:19 -0500

    implement first stack and screen

--- App.js
+++ App.js
@@ -8,10 +8,11 @@
 
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import CoinsStack from './src/components/coins/CoinsStack';
 const App: () => Node = () => {
   return (
     <NavigationContainer>
-      
+      <CoinsStack/>
     </NavigationContainer>
   );
 };
---
+++ src/components/coins/CoinsScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+const CoinsScreen = () => {
+    return(
+        <View>
+            <Text>Coins Screen</Text>
+        </View>
+    )
+}
+
+export default CoinsScreen
