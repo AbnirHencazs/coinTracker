e1981b391bc7b0f149978b723fb4b5efa53d9fc4
Brian Sanchez <briansanchezizq@gmail.com>
2021-06-25 13:34:13 -0500

    refactor design for search

--- src/components/coins/CoinsSearch.js
+++ src/components/coins/CoinsSearch.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { TextInput, Platform, View  } from 'react-native'
+import tw from 'tailwind-react-native-classnames'
 
 const CoinsSearch = ({handleChange, query}) => {
     
+    
     return(
-            <View>
+            <View style={tw`w-full items-center shadow-lg`}>
                 <TextInput
+                    style={tw`pl-4 w-full text-white rounded-lg border-2 border-gray-900`}
                     onChangeText={handleChange}
                     value={query}
                     placeholder={"Search coin"}
