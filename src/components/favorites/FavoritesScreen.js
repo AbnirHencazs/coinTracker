e597d1a44799d7dd65f32de6674a0d03f554eeb1
Brian Sanchez <briansanchezizq@gmail.com>
2021-06-28 10:03:01 -0500

    refactor design for FavoritesScreen

--- src/components/favorites/FavoritesScreen.js
+++ src/components/favorites/FavoritesScreen.js
@@ -1,15 +1,47 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, Text, FlatList } from 'react-native'
 import tw from 'tailwind-react-native-classnames'
 import FavoritesEmpty from './FavoritesEmpty'
+import Storage from '../../libs/storage'
+import CoinItem from '../coins/CoinItem'
 
-const FavoritesScreen = ({favorites}) => {
+const FavoritesScreen = ({navigation}) => {
+
+    const [ favorites, setFavorites ] = useState([])
+
+    useEffect( () => {
+        getFavorites()
+    }, [] )
+
+    const getFavorites = async () => {
+        try {
+            const allKeys = await Storage.instance.getKeys()
+            const favKeys = allKeys.filter((key) => key.includes("favorite-"))
+            const favs = await Storage.instance.list(favKeys)
+            const favorites = favs.map( fav => JSON.parse( fav[1] ) )
+            console.log(favorites)
+            setFavorites(favorites)
+        } catch (error) {
+            console.error("get favorites error", error)
+        }
+    }
+    const handlePress = (coin) => {
+        navigation.navigate("CoinDetail", {coin})
+    }
     return(
         <View style={tw`flex-1 bg-gray-800`}>
             {
-                favorites === undefined ?
+                favorites.length ?
+                <FlatList
+                    data={favorites}
+                    renderItem={({item}) =>
+                        <CoinItem
+                            item={item}
+                            handlePress={ () => handlePress(item) }/>
+                        }
+                    />
+                :
                 <FavoritesEmpty/>
-                :<Text>Tienes favoritos</Text>
             }
         </View>
     )
